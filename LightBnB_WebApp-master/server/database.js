4da85de4c01cdc68aaa18e2121d49ec86673a954
Karen <to.karenngo@gmail.com>
2022-01-06 00:16:35 -0500

    Replace boilerplate function code with query code to getUserWithEmail -allowing users to login, getUserWithID to refresh page and stay logged in, addUser to add a user to database and remain logged in

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,18 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+pool.connect(()=> {
+  console.log('connected to the database');
+});
 /// Users
 
 /**
@@ -9,18 +21,38 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
+
+  return pool
+  .query(`SELECT * FROM users WHERE users.email = $1`, [email])
+  .then((result) => { 
+    return result.rows[0] || null //you need to do [0] because otherwise it will return an array with the object and not be able to access the object.key pairs
+    //console.log(result.rows || null)
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 }
 exports.getUserWithEmail = getUserWithEmail;
+
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY NOT NULL,
+//   name VARCHAR(255) NOT NULL,
+//   email VARCHAR(255) NOT NULL,
+//   password VARCHAR(255) NOT NULL
+// );
+
+
 
 /**
  * Get a single user from the database given their id.
@@ -28,7 +60,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+  .then((result) => result.rows[0])
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
+  
+  
+  //return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -39,10 +80,22 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+
+  return pool
+  .query(`INSERT INTO users (name, email, password)
+          VALUES ($1, $2, $3)
+          RETURNING *;`, [user.name, user.email, user.password]) //RETURNING * to the end of an INSERT query to return the objects that were inserted ()
+  .then((result) => result.rows[0])
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+
+
 }
 exports.addUser = addUser;
 
@@ -54,7 +107,36 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  
+  return pool
+  .query(`SELECT * FROM reservations WHERE guest_id = $1`, [guest_id])
+  .then((result) => result.rows[0])
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+  //---- return records of reservations related to that specific user
+  
+  //return getAllProperties(null, 2);
+  
+  
+  // CREATE TABLE reservations (
+  //   id SERIAL PRIMARY KEY NOT NULL,
+  //   start_date DATE NOT NULL,
+  //   end_date DATE NOT NULL,
+  //   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  //   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  // );
+  
+  // CREATE TABLE users (
+  //   id SERIAL PRIMARY KEY NOT NULL,
+  //   name VARCHAR(255) NOT NULL,
+  //   email VARCHAR(255) NOT NULL,
+  //   password VARCHAR(255) NOT NULL
+  // );
+
+
+
 }
 exports.getAllReservations = getAllReservations;
 
@@ -66,14 +148,25 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+
+
+
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
 exports.getAllProperties = getAllProperties;
+
+
+
+
+
 
 
 /**
